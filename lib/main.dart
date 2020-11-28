3a3a50dfb751339690a58ade37d61aaac92c7ba7
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-28 16:06:56 +0600

    d

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:upgradegame/screens/homePage/homePage.dart';
+import 'package:upgradegame/screens/initPage/signInPage.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: MyHomePage(),
+      theme: ThemeData.dark(),
+      initialRoute: SignInPage.id,
+      routes: {
+        SignInPage.id: (context) => SignInPage(),
+        MyHomePage.id: (context) => MyHomePage(),
+      },
     );
   }
 }
