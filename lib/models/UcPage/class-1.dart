31905898b8bf0741533f841b4e0ed8fd1bd0f073
unknown <romanch.haq868@gmail.com>
2020-11-29 11:56:19 +0600

    lll

---
+++ lib/models/UcPage/class-1.dart
@@ -0,0 +1,36 @@
+class Dmd {
+  String amount;
+  String price;
+
+  Dmd({
+    this.price,
+    this.amount,
+  });
+}
+
+List diamonds = [
+  Dmd(
+    amount: "60 UC",
+    price: "0.99 USD or 100 coin",
+  ),
+  Dmd(
+    amount: "300 UC ",
+    price: "4.99 USD or 100 coin",
+  ),
+  Dmd(
+    amount: "600 UC",
+    price: "9.99 Usd or 100 coin",
+  ),
+  Dmd(
+    amount: "1500 UC",
+    price: "24.99 Usd or 100 coin",
+  ),
+  Dmd(
+    amount: "3000 UC",
+    price: "40.99 Usd or 100 coin",
+  ),
+  Dmd(
+    amount: "6000 UC",
+    price: "99.99 Usd or 100 coin",
+  ),
+];
