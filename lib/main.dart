86b1c1a9fdd259807a069761f89a5483d9bc7631
RomjanHossain <romjanhossain726526@gmail.com>
2020-11-29 15:29:24 +0600

    muliprovider added

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:upgradegame/screens/homePage/homePage.dart';
 import 'package:upgradegame/screens/initPage/signInPage.dart';
 import 'package:upgradegame/screens/topUpPgae/topUpPage.dart';
+import 'package:upgradegame/services/Provider/gameDataProvide.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      initialRoute: SignInPage.id,
-      routes: {
-        SignInPage.id: (context) => SignInPage(),
-        MyHomePage.id: (context) => MyHomePage(),
-        Topup.id: (context) => Topup(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => Data()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData.dark(),
+        initialRoute: SignInPage.id,
+        routes: {
+          SignInPage.id: (context) => SignInPage(),
+          MyHomePage.id: (context) => MyHomePage(),
+          Topup.id: (context) => Topup(),
+        },
+      ),
     );
   }
 }
